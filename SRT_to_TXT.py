2636c5f8321f1e7afb8bfd552a579f1cd7ceba5b
cybytess <87681509+cybytess@users.noreply.github.com>
2021-07-25 01:50:43 +0800

    SRT视频字幕 --> TXT文本
    修复了一些已知问题

--- SRT_to_TXT.py
+++ SRT_to_TXT.py
@@ -9,7 +9,7 @@ def convert(source_file_location,destination_file_location):
     sub2 = open(destination_file_location,"a",encoding="utf-8")
     sub = file.readlines()
     sub[0]=str(0)
-    file.close
+    file.close()
     a= 0
     for n in sub:
         n=n.strip('\n')
@@ -19,4 +19,4 @@ def convert(source_file_location,destination_file_location):
         a +=1
     for a in range(len(content_list)-1):
         sub2.write(content_list[a])
-    sub2.close
+    sub2.close()

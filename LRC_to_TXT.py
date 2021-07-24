b6ece1a907523c2cd3223b73ee221698abe75af6
cybytess <87681509+cybytess@users.noreply.github.com>
2021-07-25 01:46:15 +0800

    LRC歌词 --> TXT文本
    修复了一些已知问题

--- LRC_to_TXT.py
+++ LRC_to_TXT.py
@@ -10,14 +10,14 @@ def convert(source_file_location,destination_file_location):
     sub = file.readlines()
     sub[0]=sub[0][2:len(sub[0])+1]
     sub.append(sub[len(sub)-1])
-    file.close
+    file.close()
     for n in sub:
         n = n.strip(' ')
         content = n.split("]")[1]
         content_list.append(content)
     for a in range(len(content_list)-1):
                 sub2.write(content_list[a])
-    sub2.close
+    sub2.close()
 
 
 

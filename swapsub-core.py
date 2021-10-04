e075edf88753f368284060b254e4d30c3b660650
cybytess <87681509+cybytess@users.noreply.github.com>
2021-10-04 21:59:21 +0800

    Update swapsub-core.py

--- swapsub-core.py
+++ swapsub-core.py
@@ -21,4 +21,38 @@ def load(location):
             content_list.append(content)
         finallist = [hh_list,ss_list,mms_list,content_list]
         return(finallist)
-print(load("test.lrc"))
+    
+    
+    if location.split(".")[len(location.split("."))-1]=="srt":
+        hh_list = []
+        mm_list = []
+        ss_list = []
+        mms_list = []
+        content_list = []
+        file = open(location,"r",encoding="utf-8")
+        sub = file.readlines()
+        sub[0]=str(0)
+        file.close()
+        a= 0
+        for n in sub:
+            n=n.strip('\n')
+            print (n)
+            if n.isdecimal() :
+                print ("uuuuu")
+                time = sub[int(n)+1].split(' --> ')[0]
+                hh = str((time.split(':')[0]))
+                print (time)
+                print(n)
+                mm = str((time.split(':')[1]))
+                ss = str(time.split(',')[0].split(':')[2])
+                mms =  str(time.split(',')[1])
+                content = sub[a+2]
+                mm_list.append(mm)
+                ss_list.append(ss)
+                mms_list.append(mms)
+                content_list.append(content)
+        a +=1
+        print(a)
+    finallist = [hh_list,ss_list,mms_list,content_list]
+    return(finallist)
+print(load("test2.srt"))

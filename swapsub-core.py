52668426d4eabcb27952eb867e50395ae26f0dba
cybytess <87681509+cybytess@users.noreply.github.com>
2021-11-14 01:50:25 +0800

    Update swapsub-core.py

--- swapsub-core.py
+++ swapsub-core.py
@@ -1,32 +1,48 @@
 def load(path):
-    hh_list = []
-    mm_list = []
-    ss_list = []
-    mms_list = []
+    hh_starts = []
+    mm_starts = []
+    ss_starts = []
+    mms_starts = []
     content_list = []
+    hh_ends = []
+    mm_ends = []
+    ss_ends = []
+    mms_ends = []
+    file = open(path, "r", encoding="utf-8")
+    sub = file.readlines()
+    file.close()
+
     if path.split(".")[len(path.split(".")) - 1] == "lrc":
-        file = open(path, "r", encoding="utf-8")
-        sub = file.readlines()
-        sub[0] = sub[0][2:len(sub[0]) + 1]
-        file.close()
+        a = 0
         for n in sub:
             n = n.strip(" ")
             n = n.strip("[")
             content = n.split("]")[1]
             time = n.split("]")[0]
-            hh_list.append(str(int(time.split(':')[0]) // 60).rjust(2, '0'))
-            mm_list.append(str(int(time.split(':')[0]) % 60).rjust(2, '0'))
-            ss_list.append(str(time.split(':')[1].split('.')[0]).rjust(2, '0'))
-            mms_list.append(str(time.split(':')[1].split('.')[1]).ljust(3, '0'))
+            hh_starts.append(str(int(time.split(':')[0]) // 60).rjust(2, '0'))
+            mm_starts.append(str(int(time.split(':')[0]) % 60).rjust(2, '0'))
+            ss_starts.append(str(time.split(':')[1].split('.')[0]).rjust(2, '0'))
+            mms_starts.append(str(time.split(':')[1].split('.')[1]).ljust(3, '0'))
             content_list.append(content)
-        final_list = [hh_list, mm_list, ss_list, mms_list, content_list]
+            
+            if a == len(sub)-1:
+                timenext = sub[a].strip("[").split("]")[0]
+                ss_ends.append(str(int(timenext.split(':')[1].split('.')[0])+3).rjust(2, '0'))
+            else:               
+                timenext = sub[a+1].strip("[").split("]")[0]
+                ss_ends.append(str(timenext.split(':')[1].split('.')[0]).rjust(2, '0'))
+            hh_ends.append(str(int(timenext.split(':')[0]) // 60).rjust(2, '0'))
+            mm_ends.append(str(int(timenext.split(':')[0]) % 60).rjust(2, '0'))
+            mms_ends.append(str(timenext.split(':')[1].split('.')[1]).ljust(3, '0'))
+    
+            a = a+1
+            
+        final_list = [hh_starts, mm_starts, ss_starts, mms_starts,
+                      hh_ends, mm_ends, ss_ends, mms_ends,
+                      content_list]
         return final_list
 
     elif path.split(".")[len(path.split(".")) - 1] == "srt":
-        file = open(path, "r", encoding="utf-8")
-        sub = file.readlines()
-        sub[0] = str(0)
-        file.close()
         a = 0
         for n in sub:
             n = n.strip('\n')
@@ -37,66 +53,59 @@ def load(path):
                 ss = str(time.split(',')[0].split(':')[2])
                 mms = str(time.split(',')[1])
                 content = sub[a + 2]
-                hh_list.append(hh)
-                mm_list.append(mm)
-                ss_list.append(ss)
-                mms_list.append(mms)
+                hh_starts.append(hh)
+                mm_starts.append(mm)
+                ss_starts.append(ss)
+                mms_starts.append(mms)
                 content_list.append(content)
+                
+                time = sub[int(a) + 1].split(' --> ')[1]
+                hh = str((time.split(':')[0]))
+                mm = str((time.split(':')[1]))
+                ss = str(time.split(',')[0].split(':')[2])
+                mms = str(time.split(',')[1].strip("\n"))
+                hh_ends.append(hh)
+                mm_ends.append(mm)
+                ss_ends.append(ss)
+                mms_ends.append(mms)
+                
             a = a + 1
-    final_list = [hh_list, mm_list, ss_list, mms_list, content_list]
-    return final_list
+        final_list = [hh_starts, mm_starts, ss_starts, mms_starts,
+                      hh_ends, mm_ends, ss_ends, mms_ends,
+                      content_list]
+        return final_list
 
 
 def convert(data, path):
-    if path.split(".")[len(path.split(".")) - 1] == "txt":
-        content_list = data[4]
-        print(content_list)
-        file = open(path, "w", encoding="utf-8")
+    hh_starts = data[0]
+    mm_starts = data[1]
+    ss_starts = data[2]
+    mms_starts = data[3]
+    hh_ends = data[4]
+    mm_ends = data[5]
+    ss_ends = data[6]
+    mms_ends = data[7]
+    content_list = data[8]
+    file = open(path, "w", encoding="utf-8")
+    
+    if path.split(".")[len(path.split(".")) - 1] == "txt":       
         for a in content_list:
             file.write(a)
-        file.close()
+        
 
     if path.split(".")[len(path.split(".")) - 1] == "lrc":
-        mm_list = data[1]
-        ss_list = data[2]
-        mms_list = data[3]
-        content_list = data[4]
-        file = open(path, "w", encoding="utf-8")
-        for a in range(len(mm_list)):
-            print(a)
-            file.write('[' + mm_list[a] + ':' + ss_list[a] + '.' + mms_list[a] + ']' + content_list[a])
-        file.close()
+        for a in range(len(mm_starts)):
+            file.write('[' + mm_starts[a] + ':' + ss_starts[a] + '.' + mms_starts[a] + ']' + content_list[a])
 
     if path.split(".")[len(path.split(".")) - 1] == "srt":
-        hh_list = data[0]
-        mm_list = data[1]
-        ss_list = data[2]
-        mms_list = data[3]
-        content_list = data[4]
-        file = open(path, "w", encoding="utf-8")
-        hh_list.append(hh_list[len(hh_list) - 1])
-        mm_list.append(mm_list[len(mm_list) - 1])
-        ss_list.append(int(ss_list[len(ss_list) - 1]) + 5)
-        mms_list.append(mms_list[len(mms_list) - 1])
-        for a in range(len(hh_list) - 1):
+        for a in range(len(hh_starts)):
             file.write(str(a) + '\n')
-            file.write(str(hh_list[a]) + ':' + str(mm_list[a]) + ':' + str(ss_list[a]) + ','
-                       + str(mms_list[a]) + " --> " + str(hh_list[a + 1]) + ':' + str(mm_list[a + 1])
-                       + ':' + str(ss_list[a + 1]) + ',' + str(mms_list[a + 1]) + '\n')
+            file.write(str(hh_starts[a]) + ':' + str(mm_starts[a]) + ':' + str(ss_starts[a]) + ','
+                       + str(mms_starts[a]) + " --> " + str(hh_ends[a]) + ':' + str(mm_ends[a])
+                       + ':' + str(ss_ends[a]) + ',' + str(mms_ends[a]) + '\n')
             file.write(content_list[a] + '\n')
-        file.close()
 
     if path.split(".")[len(path.split(".")) - 1] == "ass":
-        hh_list = data[0]
-        mm_list = data[1]
-        ss_list = data[2]
-        mms_list = data[3]
-        content_list = data[4]
-        file = open(path, "w", encoding="utf-8")
-        hh_list.append(hh_list[len(hh_list) - 1])
-        mm_list.append(mm_list[len(mm_list) - 1])
-        ss_list.append(int(ss_list[len(ss_list) - 1]) + 5)
-        mms_list.append(mms_list[len(mms_list) - 1])
         file.write("[Script Info]\nTitle: <untitled>\nScriptType: v4.00+\n"
                     "Collisions: Normal\n"
                    "PlayDepth: 0\n\n"
@@ -106,17 +115,13 @@ def convert(data, path):
                    "Shadow, Alignment, MarginL, MarginR, MarginV, Encoding\nStyle: Default,Arial,20,&H00FFFFFF,"
                    "&H000080FF,&H00000000,&H80000000,0,0,0,0,100,100,0,0,1,2,2,2,10,10,20,0\n\n"
                    "[Events]\nFormat: Layer, Start, End, Style, Actor, MarginL, MarginR, MarginV, Effect, Text\n")
-        print(ss_list)
-        for a in range(len(hh_list) - 1):
+        for a in range(len(hh_starts)):
             file.write("Dialogue:")
             file.write("0,")
             file.write(
-                str(int(hh_list[a]) + 0) + ":" + str(mm_list[a]) + ":" + str(ss_list[a]) + "." + str(
-                    mms_list[a]) + "," +
-                str(int(hh_list[a+1]) + 0) + ":" + str(mm_list[a+1]) + ":" + str(ss_list[a+1]) + "." + str(mms_list[a+1])
+                str(int(hh_starts[a]) + 0) + ":" + str(mm_starts[a]) + ":" + str(ss_starts[a]) + "." + str(
+                    mms_starts[a]) + "," +
+                str(int(hh_ends[a]) + 0) + ":" + str(mm_ends[a]) + ":" + str(ss_ends[a]) + "." + str(mms_ends[a])
                 + ",Default,,0,0,0,," + content_list[a])
-        file.close()
+    file.close()
 
-
-subs = load("test1.srt")
-convert(subs, "test2.ass")

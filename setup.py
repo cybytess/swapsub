a837df1d829228a80130c5c8eaf360d2613c972c
cybytess <87681509+cybytess@users.noreply.github.com>
2022-01-02 02:34:16 +0800

    准备发布到PyPi

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="swapsub",
+    version="2.0.1",
+    author="cybytess",
+    author_email="p2002817@outlook.com",
+    description="a subtitle file format converter",
+
+    url="https://github.com/cybytess/swapsub", 
+
+    packages=find_packages(),
+    
+    classifliers = [
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3 ',
+        'Topic :: Multimedia :: Sound/Audio',
+        'Topic :: Multimedia :: Video',
+        'Intended Audience :: Customer Service',
+        'Intended Audience :: Developers'
+        ]
+    
+    
+)

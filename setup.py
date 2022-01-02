00198d04fd8956a0bed1f76330a9e0bdb6134452
cybytess <87681509+cybytess@users.noreply.github.com>
2022-01-02 16:05:18 +0800

    Get ready for PyPi

--- setup.py
+++ setup.py
@@ -1,18 +1,23 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+  long_description = f.read()
+
 setup(
     name="swapsub",
-    version="2.0.1",
+    version="2.0.2",
     author="cybytess",
     author_email="p2002817@outlook.com",
     description="a subtitle file format converter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
 
     url="https://github.com/cybytess/swapsub", 
 
-    packages=find_packages(),
+    packages=find_packages(where='swapsub/'),
     
-    classifliers = [
-        'Development Status :: 3 - Alpha',
+    classifiers = [
+        'Development Status :: 4 - Beta',
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 ',
         'Topic :: Multimedia :: Sound/Audio',

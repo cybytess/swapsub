54222e466a7817ea68946bdebc04c9b7f282c052
cybytess <87681509+cybytess@users.noreply.github.com>
2022-02-01 11:37:54 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="swapsub",
-    version="2.1",
+    version="2.1rc0",
     author="cybytess",
     author_email="p2002817@outlook.com",
     description="a subtitle file format converter",
@@ -11,15 +11,15 @@ setup(
 
     packages=find_packages(),
     
-    classifiers = [
-        'Development Status :: 3 - Alpha',
+    classifiers =
+    [
         'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
         'Programming Language :: Python :: 3 ',
         'Topic :: Multimedia :: Sound/Audio',
         'Topic :: Multimedia :: Video',
         'Intended Audience :: Customer Service',
         'Intended Audience :: Developers'
-        ]
+    ]
     
     
 )
